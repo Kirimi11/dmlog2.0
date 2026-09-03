baed6976cd10085c4ba36a3f44699cbd5557a6ca
Kirimi11 <94500759+Kirimi11@users.noreply.github.com>
2026-09-03 09:11:56 +0900

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import BattleRecord from "./BattleRecord.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BattleRecord />
-  </React.StrictMode>
-);
